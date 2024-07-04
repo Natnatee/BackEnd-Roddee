b0465e194915acb9a9adeabc483de979da7038e9
Tranfersafety <ratthaphumi.safe@gmail.com>
2024-07-04 17:54:06 +0700

    add function search

--- src/routes/carRoute.js
+++ src/routes/carRoute.js
@@ -1,9 +1,15 @@
-import express from 'express';
-import * as carController from '../controllers/carController.js';
+import express from "express";
+import * as carController from "../controllers/carController.js";
+import Car from "../models/Car.js";
 
 const router = express.Router();
 
 // API - 1 Post
-router.post('/', carController.createCar);
+router.post("/", carController.createCar);
+router.get("/searchbar", carController.searchCar);
+
+router.get("/test", (req, res, next) => {
+  res.status(222).json({ message: "Luffy Gear 5" });
+});
 
 export default router;
--- src/services/carService.js
+++ src/services/carService.js
@@ -1,10 +1,52 @@
 // carService.js
-import Car from '../models/Car.js';
+import Car from "../models/Car.js";
 
 const createCar = async (data) => {
-    const car = new Car(data);
-    await car.save();
-    return car;
+  const car = new Car(data);
+  await car.save();
+  return car;
 };
 
-export default { createCar };
+const searchCar = async (query) => {
+  if (query === "") {
+    return { message: "not found" };
+  }
+  const searchQuery = { $or: [] };
+  let searchableFields = [];
+  console.log(typeof query);
+  if (typeof query == "string") {
+    searchableFields = [
+      "headline",
+      "brand",
+      "model",
+      "type",
+      "color",
+      "fuel",
+      "cushion",
+      "gear",
+      "pnumber",
+      "address",
+      "additionalInfo",
+      "file1",
+      "file2",
+      "file3",
+      "file4",
+      "file5",
+      "file6",
+    ];
+    searchableFields.forEach((field) => {
+      searchQuery.$or.push({ [field]: new RegExp(query, "i") });
+    });
+  } else {
+    searchableFields = ["year", "mileage", "enginecap", "seat", "price"];
+    searchableFields.forEach((field) => {
+      searchQuery.$or.push({ [field]: query });
+    });
+  }
+
+  const cars = await Car.find(searchQuery);
+  console.log(cars);
+  return cars;
+};
+
+export default { createCar, searchCar };

d3f364d8e098c87df01e45c7785bad56eb50ac35
Natnatee <natnatee_mond@hotmail.com>
2024-07-06 06:12:52 +0700

    v

--- src/config/database.js
+++ src/config/database.js
@@ -2,12 +2,11 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
-console.log("MONGODB_URI check🚢:", process.env.MONGODB_URI);
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI, {});
-    console.log("MongoDB connected");
+    console.log("MongoDB connected ✅");
   } catch (error) {
     console.error("Error connecting to MongoDB", error);
     process.exit(1);

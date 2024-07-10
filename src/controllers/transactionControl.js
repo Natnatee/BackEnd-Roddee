039aade121446bf329dd2892e6039440be3c2380
Guitar <kittipong.sataya@gmail.com>
2024-07-11 00:23:26 +0700

    test

--- src/controllers/transactionControl.js
+++ src/controllers/transactionControl.js
@@ -8,27 +8,51 @@ const deleteTransaction = async (req, res, next) => {
   res.status(200).json({ message: "delete complete", data: result });
 };
 
-const addTransaction = async (req, res, next) => {
-  const id = req.params;
-  console.log("id at control", id);
-  const result = await transactionService.addTransaction(id);
-  res.status(200).json({ message: "B", data: result });
-};
-
 
 // Order Complete
-const createTransaction = async (req, res, next) => {
+// const createTransaction = async (req, res, next) => {
   
-  try {
-    const { Product_Id, Purchase_User } = req.body;
-    const transaction = await transactionService.createTransaction({Product_Id, Purchase_User});
+//   try {
+//     const { Product_Id, Purchase_User } = req.body;
+//     const transaction = await transactionService.createTransaction({Product_Id, Purchase_User});
     
-    res.status(201).json({message: "Order Complete", transaction});
+//     res.status(201).json({message: "Order Complete", transaction});
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// };
+
+
+const createTransaction = async (req, res, next) => {
+  try {
+    const {
+      Product_Id,
+      Sell_Price,
+      Purchase_Price,
+      Sell_Date,
+      Purchase_User,
+      Seller_User,
+    } = req.body;
+    const data = {
+      Product_Id,
+      Sell_Price,
+      Purchase_Price,
+      Sell_Date,
+      Purchase_User,
+      Seller_User,
+    };
+    const transaction = await transactionService.createTransaction(data);
+
+    res.status(201).json({ message: "Order Complete", data: transaction });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(error);
   }
 };
 
+
+
+
+// แก้ไขวันที่รับรถ
 const patchUpdate = async(req,res,next)=>{
   try {
     const id = req.params.id;
@@ -43,4 +67,4 @@ const patchUpdate = async(req,res,next)=>{
   }
 }
 
-export default { deleteTransaction, createTransaction,patchUpdate,addTransaction};
+export default { deleteTransaction, createTransaction,patchUpdate };

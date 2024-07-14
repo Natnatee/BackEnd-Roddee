aef1fafd5a6d43aec0aaabe19aa19a30827689f8
Natnatee <natnatee_mond@hotmail.com>
2024-07-15 00:55:29 +0700

    v

--- src/routes/temporaryCarRoute.js
+++ src/routes/temporaryCarRoute.js
@@ -1,13 +1,15 @@
 import express from "express";
 import temporaryCarController from "../controllers/temporaryCarController.js";
+import adminAuthenticateMiddleware from "../middleware/adminAuthenticateMiddleware.js"
+import authenticateMiddleware from "../middleware/authenticateMiddleware.js"
 
 const router = express.Router();
 
-router.get("/:id", temporaryCarController.getCarById);
+router.get("/:id",authenticateMiddleware, temporaryCarController.getCarById);
 
 // delete temp
 router.delete("/:id", temporaryCarController.deleteCarById);
 
-router.post("/",temporaryCarController.postCar)
+router.post("/",authenticateMiddleware, temporaryCarController.postCar)
 
 export default router;
